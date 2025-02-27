a96ac312302dd5f34de3426c2d362a29ec1ab14b
Agustín Amaya <80552327+bluehat8@users.noreply.github.com>
2025-02-27 13:01:16 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: 'build', // Cambiar el directorio de salida a 'build'
+  },
   server: {
     watch: {
       usePolling: true, // Useful for debugging in some environments
